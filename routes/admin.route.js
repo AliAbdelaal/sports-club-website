d987ba7a0c55c451a5df9fd2a70185a26f8c5299
Ali Abdel Aal <aliabdelaal369@gmail.com>
2019-12-04 23:19:10 +0200

    fixed typo in admin route

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,42 +1,42 @@
 const router = require('express').Router();
 const authRouter = require('./admin.auth.route');
 const errorMiddleware = require('../middleware/trainee.error.middleware');
-const verifyTokenMiddleware = require('../middleware/admin.verifytoken.middleware');
+const verifyTokenMiddleware = require('../middleware/admin.verifyToken.middleware');
 const path = require('path');
-const adminViews = "admin"+path.sep;
+const adminViews = "admin" + path.sep;
 
 
-router.use('/auth',authRouter);
+router.use('/auth', authRouter);
 
-router.get('/register',async (req,res) =>{
+router.get('/register', async(req, res) => {
     let errors = await req.consumeFlash('errors');
-    if(errors){
+    if (errors) {
         errors = errors[0];
     }
     console.log(errors);
-    res.render(traineeViews+'registration',{layout:false,errors:errors?errors:null});
+    res.render(traineeViews + 'registration', { layout: false, errors: errors ? errors : null });
 });
 
-router.get('/signin',async (req,res)=>{
+router.get('/signin', async(req, res) => {
     let errors = await req.consumeFlash('errors');
-    if(errors){
+    if (errors) {
         errors = errors[0];
     }
     console.log(errors);
-    res.render(adminViews+'login',{layout:false,errors:errors?errors:null});
+    res.render(adminViews + 'login', { layout: false, errors: errors ? errors : null });
 });
 
 router.use(verifyTokenMiddleware);
 
-router.get('/dashboard', async(req,res) =>{
+router.get('/dashboard', async(req, res) => {
     console.log(req.signedCookies['token']);
     console.log('nice sign in');
-    res.render(adminViews+'panel',{layout:false});
+    res.render(adminViews + 'panel', { layout: false });
 });
 
-router.get('/',(req,res)=>{
-    res.render(adminViews+'panel',{layout:false});
- });
+router.get('/', (req, res) => {
+    res.render(adminViews + 'panel', { layout: false });
+});
 
 router.use(errorMiddleware);
 

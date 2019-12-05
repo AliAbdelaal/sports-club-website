301bf2d4a50b1262bb61acfb3fba4cfdaf8fdb4a
khaledosama999 <khaled_bibo@hotmail.com>
2019-12-05 18:20:27 +0200

    added db config env variables

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
- db:'sports_club',
- username:'root',
- password:'',
+ db: process.env.dbName || 'W1V6rEewLS',
+ username: process.env.dbUsername ||'W1V6rEewLS',
+ password: process.env.dbPassword || 'xzbc2OAMaO',
  dialect:'mysql',
- host:'localhost',
- port:3308
+ host: process.env.host || 'remotemysql.com',
+ port: process.env.dbPort || 3306
 }
